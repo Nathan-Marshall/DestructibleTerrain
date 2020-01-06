192b3a9d79f79f384fd0d8452bfed5b5e09903a6
Nathan Marshall <NathanMarshall@cmail.carleton.ca>
2020-01-05 23:45:38 -0500

    Move clipper polygroup creation code to a new static class called Polygrouper

--- Assets/Scripts/DestrictubleTerrain/Clipping/ClipperSubtractor.cs
+++ Assets/Scripts/DestrictubleTerrain/Clipping/ClipperSubtractor.cs
@@ -51,14 +51,7 @@ namespace DestructibleTerrain.Clipping
             clipper.Execute(ClipType.ctDifference, clipperOutput, PolyFillType.pftEvenOdd, PolyFillType.pftPositive);
 
             // Convert Polytree into list of DTPolygons
-            List<DTPolygon> output = new List<DTPolygon>();
-            foreach (var poly in clipperOutput.Childs) {
-                List<Vector2> contour = poly.Contour.ToVector2List();
-                List<List<Vector2>> holes = poly.Childs.Select(hole => hole.Contour.ToVector2List()).ToList();
-                output.Add(new DTPolygon(contour, holes));
-            }
-
-            return output;
+            return clipperOutput.ToDTPolygons();
         }
         
         public List<List<DTPolygon>> SubtractPolyGroup(IEnumerable<DTPolygon> inputPolyGroup, IEnumerable<DTPolygon> clippingPolygons) {
@@ -93,35 +86,11 @@ namespace DestructibleTerrain.Clipping
             PolyTree clipperOutput = new PolyTree();
             clipper.Execute(ClipType.ctDifference, clipperOutput, PolyFillType.pftEvenOdd, PolyFillType.pftNegative);
 
-            // Construct a list of point sets to identify unique groups of connected output polygons
-            List<HashSet<IntPoint>> outputPointGroups = new List<HashSet<IntPoint>>();
-            foreach (var poly in clipperOutput.Childs) {
-                poly.MergeIntoPointGroups(outputPointGroups);
-            }
-
             // Convert Polytree into list of DTPolygons
-            List<List<DTPolygon>> output = new List<List<DTPolygon>>(outputPointGroups.Count);
-            for (int i = 0; i < outputPointGroups.Count; ++i) {
-                output.Add(new List<DTPolygon>());
-            }
-            foreach (var poly in clipperOutput.Childs) {
-                // Convert the polygon to a DTPolygon and add it to the output
-                List<Vector2> contour = poly.Contour.ToVector2List();
-                List<List<Vector2>> holes = poly.Childs.Select(hole => hole.Contour.ToVector2List()).ToList();
-                var dtPoly = new DTPolygon(contour, holes);
+            List<DTPolygon> clipperOutputDT = clipperOutput.ToDTPolygons();
 
-                // Find the correct place to put this polygon in the output structure
-                int outputGroupIndex = poly.GetFirstPointGroupIndex(outputPointGroups);
-                if (outputGroupIndex >= 0) {
-                    // Matched output group
-                    output[outputGroupIndex].Add(dtPoly);
-                } else {
-                    // New output group
-                    output.Add(new List<DTPolygon>() { dtPoly });
-                }
-            }
-
-            return output;
+            // Group the polygons into polygroups based on shared points
+            return clipperOutputDT.CreatePolygroups();
         }
 
         // WARNING: This implementation is currently very volatile and can cause multiple input polygon groups to fuse
@@ -133,7 +102,7 @@ namespace DestructibleTerrain.Clipping
             clipper.Clear();
 
             // Map the points of each polygon to the index of the polygon group to which the polygon belongs
-            Dictionary<IntPoint, int> inputPointToPolyGroup = new Dictionary<IntPoint, int>();
+            Dictionary<Vector2, int> inputPointToPolyGroup = new Dictionary<Vector2, int>(new DTUtility.ApproximateVector2Comparer());
 
             // Add subject polygon paths
             {
@@ -145,7 +114,7 @@ namespace DestructibleTerrain.Clipping
                         clipper.AddPath(contourPath, PolyType.ptSubject, true);
 
                         // Map the points to the subject group index
-                        foreach (IntPoint point in contourPath) {
+                        foreach (Vector2 point in poly.Contour) {
                             inputPointToPolyGroup[point] = inputGroupIndex;
                         }
 
@@ -175,20 +144,22 @@ namespace DestructibleTerrain.Clipping
             PolyTree clipperOutput = new PolyTree();
             clipper.Execute(ClipType.ctDifference, clipperOutput, PolyFillType.pftEvenOdd, PolyFillType.pftNegative);
 
+            List<DTPolygon> clipperOutputDT = clipperOutput.ToDTPolygons();
+
             // Construct a list of point sets to identify unique groups of connected output polygons
-            List<HashSet<IntPoint>> outputPointGroups = new List<HashSet<IntPoint>>();
-            foreach (var poly in clipperOutput.Childs) {
+            List<HashSet<Vector2>> outputPointGroups = new List<HashSet<Vector2>>();
+            foreach (var poly in clipperOutputDT) {
                 poly.MergeIntoPointGroups(outputPointGroups);
             }
 
             // Map all output groups to an input group index.
             int numInputGroups = inputPolyGroups.Count();
-            List<List<HashSet<IntPoint>>> inputOutputGroupMappings = new List<List<HashSet<IntPoint>>>(numInputGroups);
+            List<List<HashSet<Vector2>>> inputOutputGroupMappings = new List<List<HashSet<Vector2>>>(numInputGroups);
             foreach (var s in inputPolyGroups) {
-                inputOutputGroupMappings.Add(new List<HashSet<IntPoint>>());
+                inputOutputGroupMappings.Add(new List<HashSet<Vector2>>());
             }
             // Output groups that could not be mapped are in their own list.
-            List<HashSet<IntPoint>> unmappedOutputGroups = new List<HashSet<IntPoint>>();
+            List<HashSet<Vector2>> unmappedOutputGroups = new List<HashSet<Vector2>>();
             foreach (var points in outputPointGroups) {
                 int inputGroupIndex = points.GetFirstPointGroupIndex(inputPointToPolyGroup);
                 if (inputGroupIndex >= 0) {
@@ -208,31 +179,26 @@ namespace DestructibleTerrain.Clipping
                     }
                 }
             }
-            foreach (var poly in clipperOutput.Childs) {
-                // Convert the polygon to a DTPolygon and add it to the output
-                List<Vector2> contour = poly.Contour.ToVector2List();
-                List<List<Vector2>> holes = poly.Childs.Select(hole => hole.Contour.ToVector2List()).ToList();
-                var dtPoly = new DTPolygon(contour, holes);
-
+            foreach (var poly in clipperOutputDT) {
                 // Find the correct place to put this polygon in the output structure
                 int inputGroupIndex = poly.GetFirstPointGroupIndex(inputPointToPolyGroup);
                 if (inputGroupIndex >= 0) {
                     int outputGroupIndex = poly.GetFirstPointGroupIndex(inputOutputGroupMappings[inputGroupIndex]);
                     if (outputGroupIndex >= 0) {
                         // Matched input group and output group
-                        output[inputGroupIndex][outputGroupIndex].Add(dtPoly);
+                        output[inputGroupIndex][outputGroupIndex].Add(poly);
                     } else {
                         // Matched input group, new output group
-                        output[inputGroupIndex].Add(new List<DTPolygon>() { dtPoly });
+                        output[inputGroupIndex].Add(new List<DTPolygon>() { poly });
                     }
                 } else {
                     int outputGroupIndex = poly.GetFirstPointGroupIndex(unmappedOutputGroups);
                     if (outputGroupIndex >= 0) {
                         // No input group, matched output group
-                        output[inputOutputGroupMappings.Count + outputGroupIndex].Add(new List<DTPolygon>() { dtPoly });
+                        output[inputOutputGroupMappings.Count + outputGroupIndex].Add(new List<DTPolygon>() { poly });
                     } else {
                         // No input group, new output group
-                        output.Add(new List<List<DTPolygon>>() { new List<DTPolygon>() { dtPoly } });
+                        output.Add(new List<List<DTPolygon>>() { new List<DTPolygon>() { poly } });
                     }
                 }
             }
@@ -259,98 +225,14 @@ namespace DestructibleTerrain.Clipping
             return points.Select(ToVector2).ToList();
         }
 
-
-        // Returns true given group shares a point with this polygon.
-        public static bool BelongsToPointGroup(this PolyNode poly, ISet<IntPoint> points) {
-            foreach (var point in poly.Contour) {
-                if (points.Contains(point)) {
-                    return true;
-                }
+        public static List<DTPolygon> ToDTPolygons(this PolyTree tree) {
+            List<DTPolygon> output = new List<DTPolygon>();
+            foreach (var poly in tree.Childs) {
+                List<Vector2> contour = poly.Contour.ToVector2List();
+                List<List<Vector2>> holes = poly.Childs.Select(hole => hole.Contour.ToVector2List()).ToList();
+                output.Add(new DTPolygon(contour, holes));
             }
-
-            // Don't bother checking hole points
-            return false;
-        }
-
-        // Returns the index of the first group that shares a point with this polygon.
-        public static int GetFirstPointGroupIndex(this PolyNode poly, IEnumerable<ISet<IntPoint>> pointGroups) {
-            // Don't bother checking hole points
-            return poly.Contour.GetFirstPointGroupIndex(pointGroups);
-        }
-
-        // Returns the index of the first group that shares a point with this pointGroup.
-        public static int GetFirstPointGroupIndex(this IEnumerable<IntPoint> pointGroup, IEnumerable<ISet<IntPoint>> pointGroups) {
-            foreach (var point in pointGroup) {
-                int groupIndex = 0;
-                foreach (var points in pointGroups) {
-                    if (points.Contains(point)) {
-                        return groupIndex;
-                    }
-                    ++groupIndex;
-                }
-            }
-            return -1;
-        }
-
-        // Returns the index of the first group that shares a point with this polygon.
-        public static int GetFirstPointGroupIndex(this PolyNode poly, IDictionary<IntPoint, int> pointToGroup) {
-            // Don't bother checking hole points
-            return poly.Contour.GetFirstPointGroupIndex(pointToGroup);
-        }
-
-        // Returns the index of the first group that shares a point with this pointGroup.
-        public static int GetFirstPointGroupIndex(this IEnumerable<IntPoint> pointGroup, IDictionary<IntPoint, int> pointToGroup) {
-            foreach (var point in pointGroup) {
-                if (pointToGroup.ContainsKey(point)) {
-                    return pointToGroup[point];
-                }
-            }
-            return -1;
-        }
-
-        // Adds the polygon's points to the first point group that shares a point with the polygon.
-        // If any other groups share a point with the polygon, those groups are merged into the first group as well,
-        // and are removed from their previous positions in the list.
-        // Returns the index of the merged (or new) group.
-        public static int MergeIntoPointGroups(this PolyNode poly, List<HashSet<IntPoint>> pointGroups) {
-            // Find connected groups
-            List<int> connectedGroupIndices = new List<int>();
-            for (int i = 0; i < pointGroups.Count; ++i) {
-                if (poly.BelongsToPointGroup(pointGroups[i])) {
-                    connectedGroupIndices.Add(i);
-                }
-            }
-
-            if (connectedGroupIndices.Count == 0) {
-                // If this polygon is not connected to any existing point group, make a new one
-                HashSet<IntPoint> points = new HashSet<IntPoint>();
-                foreach (IntPoint point in poly.Contour) {
-                    points.Add(point);
-                }
-                // Don't bother checking hole points
-
-                pointGroups.Add(points);
-                return pointGroups.Count - 1;
-            } else {
-                // Add the polygon's points to the first connected group
-                var firstGroup = pointGroups[connectedGroupIndices[0]];
-                foreach (IntPoint point in poly.Contour) {
-                    firstGroup.Add(point);
-                }
-                // Don't bother checking hole points
-
-                // If this polygon is connected to any other groups, merge them in too
-                int numRemovals = 0;
-                for (int i = 1; i < connectedGroupIndices.Count; ++i) {
-                    int connectedGroupIndex = connectedGroupIndices[i] - numRemovals;
-
-                    firstGroup.UnionWith(pointGroups[connectedGroupIndex]);
-                    pointGroups.RemoveAt(connectedGroupIndex);
-                    ++numRemovals;
-                }
-
-                return connectedGroupIndices[0];
-            }
+            return output;
         }
     }
 }
---
+++ Assets/Scripts/DestrictubleTerrain/Polygrouper.cs
@@ -0,0 +1,132 @@
+﻿using ClipperLib;
+using DestructibleTerrain;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Polygrouper
+{
+    public static List<List<DTPolygon>> CreatePolygroups(this List<DTPolygon> polygons) {
+        // Construct a list of point sets to identify unique groups of connected output polygons
+        List<HashSet<Vector2>> outputPointGroups = new List<HashSet<Vector2>>();
+        foreach (var poly in polygons) {
+            poly.MergeIntoPointGroups(outputPointGroups);
+        }
+
+        // Use the point sets to create polygroups
+        List<List<DTPolygon>> polygroups = new List<List<DTPolygon>>(outputPointGroups.Count);
+        for (int i = 0; i < outputPointGroups.Count; ++i) {
+            polygroups.Add(new List<DTPolygon>());
+        }
+        foreach (var poly in polygons) {
+            // Find the correct place to put this polygon in the output structure
+            int outputGroupIndex = poly.GetFirstPointGroupIndex(outputPointGroups);
+            if (outputGroupIndex >= 0) {
+                // Matched output group
+                polygroups[outputGroupIndex].Add(poly);
+            }
+            else {
+                // New output group
+                polygroups.Add(new List<DTPolygon>() { poly });
+            }
+        }
+
+        return polygroups;
+    }
+
+    // Returns the index of the first group that shares a point with this polygon.
+    public static int GetFirstPointGroupIndex(this DTPolygon poly, IEnumerable<ISet<Vector2>> pointGroups) {
+        // Don't bother checking hole points
+        return poly.Contour.GetFirstPointGroupIndex(pointGroups);
+    }
+
+    // Returns the index of the first group that shares a point with this pointGroup.
+    public static int GetFirstPointGroupIndex(this IEnumerable<Vector2> pointGroup, IEnumerable<ISet<Vector2>> pointGroups) {
+        foreach (var point in pointGroup) {
+            int groupIndex = 0;
+            foreach (var points in pointGroups) {
+                if (points.Contains(point)) {
+                    return groupIndex;
+                }
+                ++groupIndex;
+            }
+        }
+        return -1;
+    }
+
+    // Returns the index of the first group that shares a point with this polygon.
+    public static int GetFirstPointGroupIndex(this DTPolygon poly, IDictionary<Vector2, int> pointToGroup) {
+        // Don't bother checking hole points
+        return poly.Contour.GetFirstPointGroupIndex(pointToGroup);
+    }
+
+    // Returns the index of the first group that shares a point with this pointGroup.
+    public static int GetFirstPointGroupIndex(this IEnumerable<Vector2> pointGroup, IDictionary<Vector2, int> pointToGroup) {
+        foreach (var point in pointGroup) {
+            if (pointToGroup.ContainsKey(point)) {
+                return pointToGroup[point];
+            }
+        }
+        return -1;
+    }
+
+    // Returns true given group shares a point with this polygon.
+    public static bool BelongsToPointGroup(this DTPolygon poly, HashSet<Vector2> points) {
+        foreach (var point in poly.Contour) {
+            if (points.Contains(point)) {
+                return true;
+            }
+        }
+
+        // Don't bother checking hole points
+        return false;
+    }
+
+    // Adds the polygon's points to the first point group that shares a point with the polygon.
+    // If any other groups share a point with the polygon, those groups are merged into the first group as well,
+    // and are removed from their previous positions in the list.
+    // Returns the index of the merged (or new) group.
+    public static int MergeIntoPointGroups(this DTPolygon poly, List<HashSet<Vector2>> pointGroups) {
+        // Find connected groups
+        List<int> connectedGroupIndices = new List<int>();
+        for (int i = 0; i < pointGroups.Count; ++i) {
+            if (poly.BelongsToPointGroup(pointGroups[i])) {
+                connectedGroupIndices.Add(i);
+            }
+        }
+
+        if (connectedGroupIndices.Count == 0) {
+            // If this polygon is not connected to any existing point group, make a new one
+            HashSet<Vector2> points = new HashSet<Vector2>(new DTUtility.ApproximateVector2Comparer());
+            foreach (Vector2 point in poly.Contour) {
+                points.Add(point);
+            }
+            // Don't bother checking hole points
+
+            pointGroups.Add(points);
+            return pointGroups.Count - 1;
+        }
+        else {
+            // Add the polygon's points to the first connected group
+            var firstGroup = pointGroups[connectedGroupIndices[0]];
+            foreach (Vector2 point in poly.Contour) {
+                firstGroup.Add(point);
+            }
+            // Don't bother checking hole points
+
+            // If this polygon is connected to any other groups, merge them in too
+            int numRemovals = 0;
+            for (int i = 1; i < connectedGroupIndices.Count; ++i) {
+                int connectedGroupIndex = connectedGroupIndices[i] - numRemovals;
+
+                firstGroup.UnionWith(pointGroups[connectedGroupIndex]);
+                pointGroups.RemoveAt(connectedGroupIndex);
+                ++numRemovals;
+            }
+
+            return connectedGroupIndices[0];
+        }
+    }
+}
