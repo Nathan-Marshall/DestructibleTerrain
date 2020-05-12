68b46a94422ae8bb99eb8f9037cdbb61322521a6
Nathan Marshall <NathanMarshall@cmail.carleton.ca>
2020-05-12 02:09:59 -0400

    Contour detection runs with no errors logged

--- Assets/Scripts/CaveGeneration/CaveGeneration.cs
+++ Assets/Scripts/CaveGeneration/CaveGeneration.cs
@@ -33,8 +33,9 @@ public class CaveGeneration : MonoBehaviour
         GenerateSurfaceHeights();
         GeneratePixelGrid();
         GenerateIntersectionGrid();
-        GeneratePixelRenderMesh();
         DetectContours(out List<List<Vector2>> solidContours, out List<List<Vector2>> holeContours);
+
+        GeneratePixelRenderMesh();
     }
 
     // Initialize random seed
@@ -247,6 +248,8 @@ public class CaveGeneration : MonoBehaviour
         GridIntersection intersection = intersectionGrid[col, row];
 
         intersection.pos = new IntPoint(col, row);
+        var p = intersection.pos.ToVec();
+        var map = intersection.contourWalkMap = new Dictionary<GridIntersection, WalkInfo>();
 
         // Get the 4 adjacent intersections
         GridIntersection left = GetIntersection(col - 1, row);
@@ -260,103 +263,79 @@ public class CaveGeneration : MonoBehaviour
         bool lt = GetCell(col - 1, row);
         bool rt = GetCell(col, row);
 
-        var map = intersection.contourWalkMap;
-        var p = intersection.pos;
-
-        // Bottom left is disconnected from the bottom right and top left, and the top right is not solid
-        if (lb != rb && lb != lt && !rt) {
-            Vector2 cornerPoint = p.ToVec();
-
-            // Unless we're at the right/top edge, there is a "\" diagonal of hole cells, so shift the corner
-            if (col < width && row < height) {
-                cornerPoint += new Vector2(-0.25f, -0.25f);
-            }
-
+        // "/" solid diagonal
+        if (lb && rt && !rb && !lt) {
+            map[left] = new WalkInfo(top, p + new Vector2(-0.25f, +0.25f), -1);
+            map[right] = new WalkInfo(bottom, p + new Vector2(+0.25f, -0.25f), -1);
+        }
+        // "\" solid diagonal
+        else if (rb && lt && !lb && !rt) {
+            map[top] = new WalkInfo(right, p + new Vector2(+0.25f, +0.25f), -1);
+            map[bottom] = new WalkInfo(left, p + new Vector2(-0.25f, -0.25f), -1);
+        }
+        // Bottom left is disconnected from the rest
+        else if (lb != rb && lb != lt && lb != rt) {
+            Vector2 cornerPoint = p + new Vector2(-0.25f, -0.25f);
             if (lb) {
                 // Bottom left is solid
                 map[left] = new WalkInfo(bottom, cornerPoint, 1);
             }
             else {
-                // Bottom right and top left are solid
+                // The rest are solid
                 map[bottom] = new WalkInfo(left, cornerPoint, -1);
             }
         }
-
-        // Bottom right is disconnected from the bottom left and top right, and the top left is not solid
-        if (rb != lb && rb != rt && !lt) {
-            Vector2 cornerPoint = p.ToVec();
-
-            // Unless we're at the left/top edge, there is a "/" diagonal of hole cells, so shift the corner
-            if (col > 0 && row < height) {
-                cornerPoint += new Vector2(+0.25f, -0.25f);
-            }
-
+        // Bottom right is disconnected from the rest
+        else if (rb != lb && rb != rt && rb != lt) {
+            Vector2 cornerPoint = p + new Vector2(+0.25f, -0.25f);
             if (rb) {
                 // Bottom right is solid
                 map[bottom] = new WalkInfo(right, cornerPoint, 1);
             }
             else {
-                // Bottom left and top right are solid
+                // The rest are solid
                 map[right] = new WalkInfo(bottom, cornerPoint, -1);
             }
         }
-
-        // Top left is disconnected from the top right and bottom left, and the bottom right is not solid
-        if (lt != rt && lt != lb && !rb) {
-            Vector2 cornerPoint = p.ToVec();
-
-            // Unless we're at the right/bottom edge, there is a "/" diagonal of hole cells, so shift the corner
-            if (col < width && row > 0) {
-                cornerPoint += new Vector2(-0.25f, +0.25f);
-            }
-
+        // Top left is disconnected from the rest
+        else if (lt != rt && lt != lb && lt != rb) {
+            Vector2 cornerPoint = p + new Vector2(-0.25f, +0.25f);
             if (lt) {
                 // Top left is solid
                 map[top] = new WalkInfo(left, cornerPoint, 1);
             }
             else {
-                // Bottom left and top right are solid
+                // The rest are solid
                 map[left] = new WalkInfo(top, cornerPoint, -1);
             }
         }
-
-        // Top right is disconnected from the top left and bottom right, and the bottom left is not solid
-        if (rt != lt && rt != rb && !lb) {
-            Vector2 cornerPoint = p.ToVec();
-
-            // Unless we're at the left/bottom edge, there is a "\" diagonal of hole cells, so shift the corner
-            if (col > 0 && row > 0) {
-                cornerPoint += new Vector2(+0.25f, +0.25f);
-            }
-
+        // Top right is disconnected from the rest
+        else if (rt != lt && rt != rb && rt != lb) {
+            Vector2 cornerPoint = p + new Vector2(+0.25f, +0.25f);
             if (rt) {
                 // Top right is solid
                 map[right] = new WalkInfo(top, cornerPoint, 1);
             }
             else {
-                // Bottom right and top left are solid
+                // The rest are solid
                 map[top] = new WalkInfo(right, cornerPoint, -1);
             }
         }
-
         // The bottom cells are solid but not the top
-        if (lb && rb && !lt && !rt) {
-            map[left] = new WalkInfo(right, p.ToVec(), 0);
+        else if (lb && rb && !lt && !rt) {
+            map[left] = new WalkInfo(right, p, 0);
         }
-
         // The top cells are solid but not the bottom
-        if (lt && rt && !lb && !rb) {
-            map[right] = new WalkInfo(left, p.ToVec(), 0);
+        else if (lt && rt && !lb && !rb) {
+            map[right] = new WalkInfo(left, p, 0);
         }
-
         // The left cells are solid but not the right
-        if (lb && lt && !rb && !rt) {
-            map[top] = new WalkInfo(bottom, p.ToVec(), 0);
+        else if (lb && lt && !rb && !rt) {
+            map[top] = new WalkInfo(bottom, p, 0);
         }
-
         // The right cells are solid but not the left
-        if (rb && rt && !lb && !lt) {
-            map[bottom] = new WalkInfo(top, p.ToVec(), 0);
+        else if (rb && rt && !lb && !lt) {
+            map[bottom] = new WalkInfo(top, p, 0);
         }
     }
 
@@ -364,13 +343,13 @@ public class CaveGeneration : MonoBehaviour
         intersectionGrid = new GridIntersection[width + 1, height + 1];
 
         for (int c = 0; c <= width; c++) {
-            for (int r = 0; r <= height + 1; r++) {
+            for (int r = 0; r <= height; r++) {
                 intersectionGrid[c, r] = new GridIntersection();
             }
         }
 
         for (int c = 0; c <= width; c++) {
-            for (int r = 0; r <= height + 1; r++) {
+            for (int r = 0; r <= height; r++) {
                 InitGridIntersection(c, r);
             }
         }
@@ -391,9 +370,7 @@ public class CaveGeneration : MonoBehaviour
                     List<Vector2> poly = new List<Vector2>();
 
                     // Set the source node to be any node CW along a contour
-                    var enumerator = start.contourWalkMap.Keys.GetEnumerator();
-                    enumerator.MoveNext();
-                    GridIntersection source = enumerator.Current;
+                    GridIntersection source = start.contourWalkMap.Keys.First();
                     GridIntersection through = start;
 
                     int turnCount = 0;
